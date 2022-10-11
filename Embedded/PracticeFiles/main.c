ea1ac10ad8ee7942b021ab0f6720028f89c836b3
Anthony Ibarra <aibarr23@uic.edu>
2022-10-11 12:29:53 -0500

    start of using PlatformIO among other commits

---
+++ Embedded/PracticeFiles/main.c
@@ -0,0 +1,18 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct 11, 2022
+ *      Author: Anthony Ibarra
+ */
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <driverlib/gpio.h>
+
+
+
+// Main function for the entirety of the project
+int main(void){
+    lpInit();
+}

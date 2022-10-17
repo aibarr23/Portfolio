1d80aa238227aed7546246172a75c7142dcf55b8
Anthony Ibarra <aibarr23@uic.edu>
2022-10-17 14:16:33 -0500

    background

--- Embedded/PlatformIO/Projects/Calculator/src/main.c
+++ Embedded/PlatformIO/Projects/Calculator/src/main.c
@@ -11,7 +11,8 @@
 
 int main(void)
 {
-
+	printf("# NOTE\nThis is running on a Tiva-C LaunchPad TM4C123G\n");
+	printf("Welcome to my personal Calculator\nType functions for a list of functions\n this calculator can do.");
 
 	
 	return 0;

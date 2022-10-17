9db8dfd89486d569faeab984be0a685267df2ac5
Anthony Ibarra <aibarr23@uic.edu>
2022-10-17 16:03:50 -0500

    update

--- Embedded/PlatformIO/Projects/Calculator/src/main.c
+++ Embedded/PlatformIO/Projects/Calculator/src/main.c
@@ -1,19 +1,27 @@
 
+#include <libopencm3/lm4f/gpio.h>
 
 #include <stdbool.h>
 #include <stdio.h>
-
-
-
+#include "launchpad.h"
 
 
 
 
 int main(void)
 {
-	printf("# NOTE\nThis is running on a Tiva-C LaunchPad TM4C123G\n");
-	printf("Welcome to my personal Calculator\nType functions for a list of functions\n this calculator can do.");
+	lpInit();
 
-	
-	return 0;
+	// printf("# NOTE\nThis is running on a Tiva-C LaunchPad TM4C123G\n");
+	// printf("Welcome to my personal Calculator\nType functions for a list of functions\n this calculator can do.");
+
+
+	// Ping out a message
+	uprintf("%s\n\r", "Hello World!");
+
+
+	while(true){
+		schdExecute();
+	}
+	// return 0;
 }

aa9e081ea252a5fd21e9752c567540d74618a50f
Anthony Ibarra <aibarr23@uic.edu>
2023-05-08 12:16:00 -0500

    update to second version

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
 console.log('test');
 
+// toggle icon navbar
+// let menuIcon = document.querySelector('#menu-icon');
+// let navbar = document.querySelector('.navbar');
+// menuIcon.onclick =() => {
+//     menuIcon.classList.toggle('bx-x');
+//     navbar.classList.toggle('active');
+// }
+
+// scroll sections
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav ul li');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            // active navbar links
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+            // active sections for animation on scroll
+            sec.classList.add('show-animate');
+        }
+        // if want to use animation that repeats on scroll use this
+        else{
+            sec.classList.remove('show-animate');
+        }
+    })
+}
+
+
+
+
+
 // ------------ sticky navbar --------------
 window.onscroll = () => {
 let header = document.querySelector('.header');
